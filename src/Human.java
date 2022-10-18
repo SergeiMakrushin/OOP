f4810d8bb75d9fd83881a0c85bd8e261be872446
Сергей <mak.sergei@ngs.ru>
2022-10-18 23:10:22 +0700

    HW OOP

---
+++ src/Human.java
@@ -0,0 +1,13 @@
+public class Human {
+
+    int yearOfBirth;
+    String name;
+    String town;
+    String job;
+
+
+    void Print() {
+        System.out.println("Привет! Меня зовут "+name+". Я из города "+town+". Я родился в "+yearOfBirth+" году. Я работаю на должности " +job+". Будем знакомы!");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Human maxim = new Human();
+        maxim.name = "Максим";
+        maxim.town = "Минск";
+        maxim.yearOfBirth= 1987;
+        maxim.job = "бренд-менеджер";
+
+
+        Human anna=new Human();
+        anna.name="Аня";
+        anna.town="Москва";
+        anna.yearOfBirth=1993;
+        anna.job = "методист образовательных програм";
+
+        Human kate = new Human();
+        kate.name = "Катя";
+        kate.town = "Калининград";
+        kate.yearOfBirth = 1994;
+        kate.job = "продакт-менеджер";
+
+        Human artem = new Human();
+        artem.name = "Артём";
+        artem.town = "Москва";
+        artem.yearOfBirth = 1995;
+        artem.job = "директор по развитию бизнеса";
+
+        maxim.Print();
+        anna.Print();
+        kate.Print();
+        artem.Print();
+
+
     }
 }
